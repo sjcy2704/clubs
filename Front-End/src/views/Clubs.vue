4822f034ff4447e0492d59ca3140ebbc54f5f523
Leo <leo.nguyen@student.adelaide.edu.au>
2023-05-12 17:59:45 +0930

    Worked on home and clubs

--- Front-End/src/views/Clubs.vue
+++ Front-End/src/views/Clubs.vue
@@ -1,3 +1,27 @@
 <template>
-  <p style="font-size: 100px">Clubs</p>
+  <div class="container">
+    <pre style="font-size: 100px">Clubs</pre>
+  </div>
+
+  <div class="clubIcon"></div>
+  <div class="clubIcon"></div>
+  <div class="clubIcon"></div>
+  <div class="clubIcon"></div>
+  
 </template>
+
+<style scoped>
+  .container {
+  text-align: center;
+  padding: 50px;
+  }
+
+  .clubIcon {
+    border: black 2px solid;
+    margin-left: 20px;
+    margin-right: 20px;
+    width: 250px;
+    height: 250px;
+    float: left;
+  }
+</style>
--- Front-End/src/views/Home.vue
+++ Front-End/src/views/Home.vue
@@ -1,3 +1,122 @@
 <template>
-  <p style="font-size: 100px">Home</p>
+  <div class="container">
+    <pre style="font-size: 100px">Welcome to 
+University name</pre>
+  </div>
+
+  <div class="news">
+    <pre style="font-size: 25px">
+News
+
+
+
+
+
+
+
+
+
+
+
+
+    </pre>
+  </div>
+<div class="clubs">
+  <pre style="font-size: 25px; text-align: center;">
+Log in to see 
+ your clubs!
+
+
+
+
+
+
+
+
+
+
+
+ 
+  </pre>
+</div>
+
+<div class="rvspBlack">
+  <div class="reDiv"><pre style="color: white">
+  Events/RVSP
+  </pre>
+  </div>
+</div>
+
+<div class="rvspWhite">
+  <div class="reDiv"><pre>
+  Event/RVSP
+  </pre>
+  </div>
+</div>
+
+<div class="rvspBlack">
+  <div class="reDiv"><pre style="color: white">
+  Events/RVSP
+  </pre>
+  </div>
+</div>
+
 </template>
+
+
+<style scoped>
+
+.reDiv {
+  font-size: 20px;
+  transform: skew(30deg);
+}
+
+.container {
+  text-align: center;
+  padding: 50px;
+}
+
+.news {
+  text-align: left;
+  border: black 2px solid;
+  border-radius: 10px;
+  width: 75%;
+  margin: auto;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.clubs {
+  border: black 2px solid;
+  border-radius: 10px;
+  width: 600px;
+  margin-left: 238px;
+  margin-top: 10px;
+  padding: 20px;
+  float: left;
+  box-sizing: content-box;
+}
+
+.rvspBlack {
+  transform: skew(-30deg);
+  border: black solid 1px;
+  background-color: black;
+  width: 290px;
+  height: 100px;
+  margin-right: 100px;
+  margin-top: 10px;
+  padding: 20px;
+  float: right;
+}
+
+.rvspWhite {
+  transform: skew(-30deg);
+  border: black solid 1px;
+  width: 290px;
+  height: 100px;
+  margin-right: 100px;
+  margin-top: 10px;
+  padding: 20px;
+  float: right;
+}
+</style>
