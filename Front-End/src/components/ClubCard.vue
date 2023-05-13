7a6d9c999a8d75228416ab411343e6b7418fa69c
sjcy2704 <a1834160@adelaide.edu.au>
2023-05-13 12:33:27 +0930

    Add club cards

---
+++ Front-End/src/components/ClubCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+  name: String,
+  category: String,
+  members: String,
+});
+</script>
+
+<template>
+  <div class="clubContainer">
+    <div class="clubIcon"></div>
+    <div class="details flex justify-between">
+      <div>
+        <p class="name">{{ name }}</p>
+        <p class="category">{{ category }}</p>
+      </div>
+      <div>
+        <p class="members">{{ members }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.details {
+  text-align: left;
+  padding: 10px 0;
+}
+
+.category {
+  font-size: 0.85em;
+}
+
+.clubIcon {
+  border: black 2px solid;
+  width: 250px;
+  height: 250px;
+}
+</style>
--- Front-End/src/views/Clubs.vue
+++ Front-End/src/views/Clubs.vue
@@ -1,22 +1,13 @@
+<script setup>
+import ClubCard from "../components/ClubCard.vue";
+</script>
 <template>
   <div class="container">
     <div class="title">Clubs</div>
   </div>
 
   <div class="cardsContainer flex flex-wrap">
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
-    <div class="clubIcon"></div>
+    <ClubCard v-for="n in 20" name="Club" category="Fitness" members="10" />
   </div>
 </template>
 
@@ -29,11 +20,5 @@
   gap: 40px;
   width: 60%;
   margin: 0 auto;
-}
-
-.clubIcon {
-  border: black 2px solid;
-  width: 250px;
-  height: 250px;
 }
 </style>
