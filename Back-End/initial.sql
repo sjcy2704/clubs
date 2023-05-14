92579696ac57e75d48e966b8e99aee9972d9dc2f
sjcy2704 <a1834160@adelaide.edu.au>
2023-05-14 16:42:27 +0930

    Change npm scripts

--- Back-End/initial.sql
+++ Back-End/initial.sql
@@ -53,3 +53,5 @@ INSERT INTO
   Clubs (name, category, manager)
 VALUES
   ('AUVC', 'Sports', 2);
+
+-- select u.userID, u.username, u.email, u.firstName, u.familyName, u.phone, u.userType, c.name from Users u join ClubMembers cm on u.userID = cm.userID join Clubs c on cm.clubID = c.clubID;
