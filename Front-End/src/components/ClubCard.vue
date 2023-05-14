10e1186fb653fd1ea440e66c92fe13dba0ef2c6a
sjcy2704 <a1834160@adelaide.edu.au>
2023-05-14 18:59:10 +0930

    Add icons

--- Front-End/src/components/ClubCard.vue
+++ Front-End/src/components/ClubCard.vue
@@ -14,8 +14,9 @@ defineProps({
         <p class="name">{{ name }}</p>
         <p class="category">{{ category }}</p>
       </div>
-      <div>
+      <div class="membersDiv flex align-center">
         <p class="members">{{ members }}</p>
+        <font-awesome-icon icon="fa-solid fa-users" />
       </div>
     </div>
   </div>
@@ -25,6 +26,14 @@ defineProps({
 .details {
   text-align: left;
   padding: 10px 0;
+}
+
+.membersDiv {
+  margin-right: 10px;
+}
+
+.members {
+  margin-right: 5px;
 }
 
 .category {
--- Front-End/src/main.js
+++ Front-End/src/main.js
@@ -5,8 +5,14 @@ import "./style.css";
 import App from "./App.vue";
 
 const pinia = createPinia();
-const app = createApp(App);
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
 
-app.use(pinia);
-app.use(router);
-app.mount("#app");
+library.add(faUsers);
+
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
