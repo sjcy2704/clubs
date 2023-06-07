c5f71d79ca6d0eba1a448c7dc0a01ee31a1c4e15
sjcy2704 <a1834160@adelaide.edu.au>
2023-06-08 05:47:09 +0930

    Remove unnecessary import

--- Front-End/src/views/Clubs.vue
+++ Front-End/src/views/Clubs.vue
@@ -1,10 +1,7 @@
 <script setup>
 import ViewClubs from "../components/ViewClubs.vue";
-import { useUserStore } from "../stores/userStore";
 import { ref } from "vue";
 import { api } from "../helpers/api";
-
-const userStore = useUserStore();
 
 let allClubs = ref([]);
 await api.get("/clubs").then(({ data }) => (allClubs.value = data));
